8460b146789a8e4ba21003647c6c1c403f903f74
Flynn <1845797+ling7334@users.noreply.github.com>
2022-03-04 09:52:13 +0800

    initial project

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # fastapi-orgs
+
+Ready-to-use and customizable organizations management for FastAPI
---
+++ fastapi-orgs/__init__.py
@@ -0,0 +1,3 @@
+"""Ready-to-use and customizable organizations management for FastAPI."""
+
+__version__ = "0.1.0"
